4f40a52c9490d22b80fc18c86da6c7aa29befe71
KevinFang97 <hfangac@connect.ust.hk>
2017-09-04 15:10:02 +0800

    Update main.py
    algo of clustering need to be decided to continue the unsupervised part

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#note:sentence_size here does not include <SOS> or <EOS>
 import torch
 import torch.nn as nn
 
@@ -14,7 +14,34 @@ UNK_token = 2
 
 MAX_SEQ_LEN = 20
 
-#-------------Train The Model------------#
+#-------------Train The Cluster Model------------#
+def cluster_train_step(q_batch)
+
+#probability dict should be calculated while generating word dict
+#here the input of embedder is a sentence(int value,  padded)
+def cluster_train(answers, embedder, answer_length, prob_dict):
+	answer_vec = []
+	a = 1 #TO BE DECIDED
+
+	
+	
+	for i in range(answers):
+		prob = [] #prob of the sentence
+		for j in range(len(answers[i])):
+			word_prob = prob_dict[answers[i][j+1]]
+			prob.append(word_prob) #finish retrieving the prob of word in this sentence
+		wr = sentence_vector_wr(embedder(answers[i]), prob, answers_length[i], a) #wr is the sentence_vec (not PCAed yet)
+		answer_vec.append(wr)
+		
+	sentence_vector_pca(answer_vec) #doing PCA for all sentence_vec of answers
+	
+	#clustering using answer_vec, ALGO TO BE DECIDED
+	
+	
+#-------------End of The Cluster Model-----------#
+
+
+#-------------Train The Main Model------------#
 
 #(Development use)List the models needed to be implemented in model.py, which are used here
 #1.
